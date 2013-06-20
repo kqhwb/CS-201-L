044490a9e90488b798e4df6d186ff36580667311
unknown <kqhwb@KC-SCE462LAB-13.kc.umkc.edu>
2013-06-20 11:44:07 -0500

    CS201L Lab1

---
+++ Lab1/Assign1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+	string myMethod;
+
+	cout << "What is your name? " << endl; 
+	cin >> myMethod;
+	cout << "Hello, " << myMethod << endl; 
+
+	system("pause");
+
+}
