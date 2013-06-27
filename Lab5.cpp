75491f8dd77dd850a0c10995b29d268bc13d6b98
unknown <kqhwb@KC-SCE462LAB-08.kc.umkc.edu>
2013-06-27 10:06:53 -0500

    Lab 5

---
+++ Lab5.cpp
@@ -0,0 +1,12 @@
+﻿'Lab5.exe': Loaded '\\kc.umkc.edu\kc-users\home\k\kqhwb\My Documents\Visual Studio 2010\Projects\Lab5\Debug\Lab5.exe', Symbols loaded.
+'Lab5.exe': Loaded 'C:\Windows\System32\ntdll.dll', Cannot find or open the PDB file
+'Lab5.exe': Loaded 'C:\Windows\System32\kernel32.dll', Cannot find or open the PDB file
+'Lab5.exe': Loaded 'C:\Windows\System32\KernelBase.dll', Cannot find or open the PDB file
+'Lab5.exe': Loaded 'C:\Windows\System32\msvcp100d.dll', Symbols loaded.
+'Lab5.exe': Loaded 'C:\Windows\System32\msvcr100d.dll', Symbols loaded.
+'Lab5.exe': Loaded 'C:\Windows\System32\advapi32.dll', Cannot find or open the PDB file
+'Lab5.exe': Loaded 'C:\Windows\System32\msvcrt.dll', Cannot find or open the PDB file
+'Lab5.exe': Loaded 'C:\Windows\System32\sechost.dll', Cannot find or open the PDB file
+'Lab5.exe': Loaded 'C:\Windows\System32\rpcrt4.dll', Cannot find or open the PDB file
+'Lab5.exe': Loaded 'C:\Windows\System32\apphelp.dll', Cannot find or open the PDB file
+The program '[6480] Lab5.exe: Native' has exited with code 0 (0x0).
